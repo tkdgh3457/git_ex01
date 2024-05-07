c5930c26b461121479105caa8e209733984f6e16
tkdgh3457 <tkdgh3457@naver.com>
2024-05-07 11:09:22 +0900

    first commit

---
+++ src/main/java/com/tjoeun/shop/GitEx01Application.java
@@ -0,0 +1,13 @@
+package com.tjoeun.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEx01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitEx01Application.class, args);
+	}
+
+}
